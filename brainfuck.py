f6d4b06c93d759aa5c328db0e5a80e85a3cc2b0c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-06-07 21:03:57 +0200

    344

--- brainfuck.py
+++ brainfuck.py
@@ -1,20 +1,19 @@
 import sys
-I=[]
-while[' ']!=I[-1:]:I+=sys.stdin.read(1)
-I=map(ord,I)
-s=i=m=0
+I,S=''.join(sys.stdin.readlines()).split(' ',1)
+i=m=0
 M=[0]*30000
+O=''
 while i<len(I):
- c=I[i];m+=c==62;m-=c==60;M[m]+=c<44;M[m]-=c==45
- if 46==c:sys.stdout.write(chr(M[m]))
- if 44==c:
-  r=sys.stdin.read(1)
-  M[m]=ord(r)if r else-1
- n,d=0,0
+ n,d=0,0;c=ord(I[i]);m+=c==62;m-=c==60;M[m]+=c<44;M[m]-=c==45
+ if 46==c:O+=chr(M[m])
+ if(44==c)*S:
+  M[m]=ord(S[0])
+  S=S[1:]
  if(c==91)*(M[m]==0):n,d=1,1
  if c>92:n,d=1,-1
  while n:
   i+=d
-  if 91==I[i]:n+=d
-  if 92<I[i]:n-=d
+  if'['==I[i]:n+=d
+  if']'==I[i]:n-=d
  i+=c<93
+print O,
