d539f21c6c0dad915d31a4fba67d85eccfbba315
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-06-07 21:26:28 +0200

    331

--- brainfuck.py
+++ brainfuck.py
@@ -1,14 +1,12 @@
 import sys
-I,S=''.join(sys.stdin.readlines()).split(' ',1)
-i=m=0
-M=[0]*30000
 O=''
-while i<len(I):
- n,d=0,0;c=ord(I[i]);m+=c==62;m-=c==60;M[m]+=c<44;M[m]-=c==45
+I,S=O.join(sys.stdin.readlines()).split(' ',1)
+n=d=i=m=0
+M=[0]*30000
+while I[i:]:
+ c=ord(I[i]);m+=c==62;m-=c==60;M[m]+=c<44;M[m]-=c==45
  if 46==c:O+=chr(M[m])
- if(44==c)*S:
-  M[m]=ord(S[0])
-  S=S[1:]
+ if(44==c)*S:M[m]=ord(S[0]);S=S[1:]
  if(c==91)*(M[m]==0):n,d=1,1
  if c>92:n,d=1,-1
  while n:
